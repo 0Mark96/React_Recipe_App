003f33e54295bbc334d0910237b48cea9b920ada
0mark96 <verardimarco3@gmail.com>
2022-06-14 11:28:53 +0200

    fixed style header and footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,9 +6,9 @@ const Footer = ()=>{
     return(
         <footer  className={styles.footer}>
             <div className={styles.projectInfo}>
-             <h3>Author: Marco Verardi</h3>
-             <h3>Project for Start2Impact</h3>
-             <h3>Project Date: 2022</h3>
+             <h2>Author: Marco Verardi</h2>
+             <h2>Project for Start2Impact</h2>
+             <h2>Project Date: 2022</h2>
             </div>
             <div className={styles.socialLinks}>
              <h2>Social Links</h2>
